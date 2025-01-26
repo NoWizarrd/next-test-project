7b5d1909cf52fe6486a8359ff6f2f38b22501750
Aleksey <pnev.leha@mail.ru>
2025-01-26 20:57:42 +0700

    add todo list

--- src/app/todos/page.tsx
+++ src/app/todos/page.tsx
@@ -1,40 +1,77 @@
-"use client"
+"use client";
+import Todos from "components/todos/Todos";
 import { useEffect, useState } from "react";
 import { Todo } from "types/TodoType";
 
-const baseUrlTodos = 'https://jsonplaceholder.typicode.com/todos'
+const baseUrlTodos = "https://jsonplaceholder.typicode.com/todos?_limit=5";
 
 export default function TodosPage() {
-    const [todos, setTodos] = useState<Todo[]>([]);
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [inputTask, setInputTask] = useState("");
 
-    const fetchTodos = async () => {
-        const response: Todo[] = await fetch(
-            baseUrlTodos, 
-            { cache: 'force-cache' })
-        .then(res => res.json())
+  const fetchTodos = async () => {
+    const response: Todo[] = await fetch(baseUrlTodos, { cache: "force-cache" })
+      .then((res) => res.json());
+    setTodos(response);
+  };
 
-        setTodos(response)
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  const addTodo = () => {
+    if (inputTask.trim()) {
+      const newTodo: Todo = {
+        id: todos.length + 1, 
+        title: inputTask,
+        completed: false,
+        userId: 1,
+      };
+      setTodos([newTodo, ...todos]); 
+      setInputTask(""); 
     }
+  };
 
-    useEffect(() => {
-        fetchTodos()
-    }, []);
+  const removeTodo = (todoId: number) => {
+    setTodos(todos.filter((todo) => todo.id !== todoId));
+  };
 
-    return(
-        <div className="flex flex-col  justify-center items-center bg-background text-foreground">
-            <h1 className="my-5 text-3xl">Страница задач</h1>
-            <ul className="text-xl">
-                {todos.map((todo)=> (
-                    <li key={todo.id} className="border-2 border-[var(--borderColor)] dark:bg-[var(--darkGray)] m-4 p-2">
-                        <div>
-                            Title: {todo.title}
-                        </div>
-                        <div>
-                            Completed: {String(todo.completed)}
-                        </div>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    )
+  const changeCompleted = (todoId: number) => {
+    const updatedTodos = todos.map((todo) =>
+      todo.id === todoId ? { ...todo, completed: !todo.completed } : todo
+    );
+    setTodos(updatedTodos);
+  };
+  
+  return (
+    <div className="flex flex-col justify-center items-center bg-background text-foreground p-5">
+      <h1 className="my-5 text-3xl">Страница задач</h1>
+      <div className="flex items-center gap-2 mb-5">
+        <input
+          value={inputTask}
+          onChange={(e) => setInputTask(e.target.value)}
+          placeholder="Введите задачу..."
+          className="
+            px-4 py-2 border rounded-lg
+            bg-white text-black dark:bg-gray-800 dark:text-white
+            focus:ring-2 focus:ring-blue-500 focus:outline-none
+            border-gray-300 dark:border-gray-600
+          "
+        />
+        <button
+          onClick={addTodo}
+          className="
+            px-4 py-2 bg-blue-500 dark:bg-gray-700 
+            text-white dark:text-[#d1d5db] rounded-lg hover:bg-blue-600 dark:hover:bg-gray-600
+            transition-colors duration-200
+          "
+        >
+          Добавить задачу
+        </button>
+      </div>
+      <ul className="text-xl">
+        { todos.length ? <Todos click={changeCompleted} removeTodo={removeTodo} todos={todos} /> : <p>Нет задач</p>}
+      </ul>
+    </div>
+  );
 }
---
+++ src/components/todos/Todos.tsx
@@ -0,0 +1,33 @@
+import React, { ReactNode } from 'react'
+import { Todo } from 'types/TodoType'
+
+interface TodosProps {
+    todos: Todo[],
+    removeTodo: (id: number) => void,
+    click: (id: number) => void
+}
+
+export default function Todos (props: TodosProps): ReactNode {
+
+    const {todos, removeTodo, click} = props
+
+    return (
+        <>
+            {todos.map((todo) => (
+                <li key={todo.id}
+                className="
+                flex relative flex-col min-w-[35vw]
+                border-2 border-[var(--borderColor)] dark:bg-[var(--darkGray)] m-4 p-3">
+                    <div className='mr-10'>
+                        Title: {todo.title}
+                    </div>
+                    <div>
+                        Completed: {String(todo.completed)}
+                    </div>
+                    <button onClick={() => removeTodo((todo.id))} className='absolute top-0 right-1 hover:text-blue-500 dark:hover:text-gray-500'>X</button>
+                    <button onClick={() => click((todo.id))} className='absolute bottom-0 right-1 hover:text-blue-500 dark:hover:text-gray-500'>Выполнить</button>
+                </li>
+            ))}
+        </>
+    )
+}
